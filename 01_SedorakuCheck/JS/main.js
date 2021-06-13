af323c57fe4ab71c07dfd8b3b730c5dceae8d05d
raise-sys <raise-sys@gmail.com>
2021-06-13 16:41:40 +0900

    aaa

---
+++ 01_SedorakuCheck/JS/main.js
@@ -0,0 +1,12 @@
+let text_form = document.getElementById('text_form');
+let output = document.getElementById('output');
+
+text_form.addEventListener('keypress', test_ivent);
+
+
+function test_ivent(e) {
+    if (e.keyCode === 13) {
+      output.innerHTML = text_form.value;
+      return false;     
+  }  
+}
